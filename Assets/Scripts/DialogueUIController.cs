8ed781bbb1372dcaa48ce6adab190b7be0648a5a
Neminda Bandara <nemindabandara15@gmail.com>
2026-09-09 12:25:40 +0530

    Created dialogue boxes and choices

---
+++ Assets/Scripts/DialogueUIController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class DialogueUIController : MonoBehaviour
+{
+  [Header("UI Elements")]
+  public GameObject panelRoot;
+  public TextMeshProUGUI speakerText;
+  public TextMeshProUGUI contentText;
+  public Transform choicesContainer;
+  public GameObject choiceButtonTemplate;
+
+  void Awake()
+  {
+    // Speaker: Milo
+    // Action/Internal POV: Physical canine cues + clear directive to the hikers
+    ShowDialogue("Milo", "*Turns back toward the hikers and gives a sharp, urgent bark.* [Obstacle ahead: Loose shale on the incline.]");
+  }
+
+  public void ShowDialogue(string speaker, string content)
+  {
+    panelRoot.SetActive(true);
+    speakerText.text = speaker;
+    contentText.text = content;
+  }
+
+  public void HideDialogue()
+  {
+    panelRoot.SetActive(false);
+  }
+}
